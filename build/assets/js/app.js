5bb2e02b29d06a8a6a725f96b218516b2a324016
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-14 21:53:32 -0700

    Started implementation of ScrollReveal

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -7,3 +7,21 @@ function showNav() {
 		navDiv.className = "responsive-nav"
 	}
 }
+
+// Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('#header #main-title', {
+	origin: 'top',
+	distance: '10px'
+});
+
+sr.reveal('#header #subtitle', {
+	origin: 'top',
+	distance: '10px'
+});
+
+
+sr.reveal('#header img.phone', { 
+	duration: 500,
+	delay: 600 
+});
