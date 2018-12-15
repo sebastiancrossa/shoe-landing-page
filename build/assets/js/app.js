5f6ef28c8218dd9ae574384179ff67a329838e55
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-14 21:36:13 -0700

    Added hamburger navigation

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,9 @@
-window.sr=ScrollReveal();
+function showNav() {
+	var navDiv = document.getElementById("responsive-nav");
+
+	if (navDiv.className == "responsive-nav") {
+		navDiv.className = "responsive-nav unfold"
+	} else {
+		navDiv.className = "responsive-nav"
+	}
+}
