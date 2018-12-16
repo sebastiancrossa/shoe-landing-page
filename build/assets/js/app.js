7aeed37bf7665338287638999c727a32ddb02f3b
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-15 22:31:58 -0700

    Fixed local issues

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,27 +1,9 @@
 function showNav() {
-	var navDiv = document.getElementById("responsive-nav");
+ 	var navDiv = document.getElementById("responsive-nav");
 
-	if (navDiv.className == "responsive-nav") {
-		navDiv.className = "responsive-nav unfold"
-	} else {
-		navDiv.className = "responsive-nav"
-	}
-}
-
-// Scroll Reveal
-window.sr = ScrollReveal();
-sr.reveal('#header #main-title', {
-	origin: 'top',
-	distance: '10px'
-});
-
-sr.reveal('#header #subtitle', {
-	origin: 'top',
-	distance: '10px'
-});
-
-
-sr.reveal('#header img.phone', { 
-	duration: 500,
-	delay: 600 
-});
+ 	if (navDiv.className == "responsive-nav") {
+ 		navDiv.className = "responsive-nav unfold"
+ 	} else {
+ 		navDiv.className = "responsive-nav"
+ 	}
+ } 
